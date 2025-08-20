a52857a54ac7b608c9dc72c48147e328be4bc33b
liluppis <lisa.upp@hotmail.com>
2025-08-20 13:56:29 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express"
+import "dotenv/config"
+
+const app = express()
+const port: number = 3000
+const secret: string | undefined = process.env.MY_GLOBAL_TEST_SECRET
+
+// defines an endpoint (/ = home)
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!")
+})
+
+// start server on 'port' variable
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+  console.log(secret) // never log sensitive information -> only for debugging
+})
