6649988c12d5000b730b0095dc3cbe93f79be656
liluppis <lisa.upp@hotmail.com>
2025-08-20 14:22:32 +0200

    add comments for further explanation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express from "express"
+import express from "express" // 'import' instead of 'required'
 import "dotenv/config"
 
 const app = express()
 const port: number = 3000
 const secret: string | undefined = process.env.MY_GLOBAL_TEST_SECRET
 
-// defines an endpoint (/ = home)
+// defines an endpoint (/ = index)
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!")
 })
